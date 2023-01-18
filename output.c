4e02f1594dce55316d42da08ca7ce3dd244e5cdd
Travis <Toooo123@live.ca>
2023-01-17 17:15:25 -0800

    Create Screen Output Module

---
+++ output.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void Output_printWelcome(void)
+{
+    printf("Hello embedded world, from Travis!\n");
+}
---
+++ output.h
@@ -0,0 +1,6 @@
+#ifndef OUTPUT_H_
+#define OUTPUT_H_
+
+void Output_printWelcome(void);
+
+#endif
