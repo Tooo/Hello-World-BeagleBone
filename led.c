9b6679e91077bafed1f5e875a4bd6aba7eac3ee8
Travis <Toooo123@live.ca>
2023-01-17 23:16:56 -0800

    Intialize led

--- led.c
+++ led.c
@@ -1,5 +1,12 @@
 #include "led.h"
 
+#include "system.h"
+
 void Led_intialize(void)
 {
+    // Set LED triggers to None
+    System_writeFile("/sys/class/leds/beaglebone:green:usr0/trigger", "none");
+    System_writeFile("/sys/class/leds/beaglebone:green:usr1/trigger", "none");
+    System_writeFile("/sys/class/leds/beaglebone:green:usr2/trigger", "none");
+    System_writeFile("/sys/class/leds/beaglebone:green:usr3/trigger", "none");
 }
